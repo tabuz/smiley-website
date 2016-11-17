7ef9f4d6b7d0f248d3aa8b9d186171323ccf6f55
tabuz <zelek.hubert@gmail.com>
2016-11-17 17:44:01 +0000

    Testimonials, clients ++

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,44 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
-    }
-  });
+  testimStuff();
+    $('#team-carousel').owlCarousel({
+      items: 3,
+    });
+    $('#clients').owlCarousel({
+      navigation: true,
+      pagination: false,
+      navigationText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    });
+
+  function testimStuff(){
+    $('.testim-unit').first().next().addClass('active-testim');
+    var i = 2,
+        content = $('.active-testim p').html();
+    $('.testimonials-caption p').html(content);
+
+      $('.next, .prev').click(function(){
+        var $this = $(this);
+
+            if($this.hasClass('next')){
+              if(i < 3){
+                $('.active-testim').removeClass('active-testim').next().addClass('active-testim');
+                content = $('.active-testim p').html();
+                i++;
+                }else{
+                  $('.testim-unit').removeClass('active-testim').first().addClass('active-testim');
+                  content = $('.active-testim p').html();
+                  i=1;
+                }
+              }else{
+                  $('.active-testim').removeClass('active-testim').prev().addClass('active-testim');
+                  content = $('.active-testim p').html();
+                  i--;
+                  if(i <= 0){
+                    $('.testim-unit').removeClass('active-testim').last().addClass('active-testim');
+                    content = $('.active-testim p').html();
+                    i=3;
+                  }
+              }
+            $('.testimonials-caption p').html(content);
+      });
+  }
 });
